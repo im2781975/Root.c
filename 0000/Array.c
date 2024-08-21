4872a7778770e2a5a4baca4dda2c61f5de300886
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-08-21 18:52:11 +0200

    Update Array.c

--- 0000/Array.c
+++ 0000/Array.c
@@ -45,3 +45,20 @@ __main(){
     for(size_t i = 0; i < SIZE; i++)
         printf("%u ", arr[i]);
 }
+
+//Find cube of x using pointer
+void cubeByRef(int *ptr){
+    *ptr = (*ptr) * (*ptr) * (*ptr);
+}
+int cubeByval(int num){
+    return num * num * num;
+}
+__main(){
+    int num; 
+    scanf("%d", &num);
+    int x = num;
+    cubeByRef(&num);
+    printf("Cube of(ByRef): %d is %d ", x, num);
+    int ans = cubeByval(num);
+    printf("\nCube of(Byval): %d is %d", num, ans);
+}
