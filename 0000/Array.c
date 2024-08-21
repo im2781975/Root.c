4896f431de91271805b976f5bc65324558df054e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-08-21 20:30:19 +0200

    Update Array.c

--- 0000/Array.c
+++ 0000/Array.c
@@ -66,3 +66,24 @@ __main(){
     for(size_t i = 0; i < SIZE; ++i)
         printf("%d ", arr[i]);
 }
+
+//print 2D
+void print(int arr[][3]){
+    for(size_t i = 0; i <= 1; i++){
+        for(size_t j = 0; j <= 2; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+__main(){
+    int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    puts("Array is: ");
+    print(arr);
+    int seq[2][3] = {1, 2, 3, 4, 5};
+    puts("Seq is: ");
+    print(seq);
+    int matrix[2][3] = {{1, 2}, {4}};
+    puts("Matrix is: ");
+    print(matrix);
+}
