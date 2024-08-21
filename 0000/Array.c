c20d0b61f787f99b6af96ee28ebaf27264af627b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-08-21 17:06:42 +0200

    Update Array.c

--- 0000/Array.c
+++ 0000/Array.c
@@ -24,3 +24,24 @@ __main(){
     for(size_t i = 0; i < SIZE; ++i)
         printf("(ptr + %u) = %d\n", (ptr + i), ptr[i]);
 }
+
+//Bubble Sort
+#define SIZE 10
+void swap(int *a, int *b){
+    int tmp = *a;
+    *a = *b; *b = tmp;
+}
+void BubbleSort(int *arr, size_t size){
+    for(unsigned int pass = 0; pass < size - 1; pass++){
+        for(size_t j = 0; j < size - 1; j++){
+            if(arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+        }
+    }
+}
+__main(){
+    int arr[SIZE] = {2, 6, 4, 8, 10, 12, 89, 68, 45, 37};
+    BubbleSort(arr, SIZE);
+    for(size_t i = 0; i < SIZE; i++)
+        printf("%u ", arr[i]);
+}
