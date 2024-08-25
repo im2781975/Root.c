43b6180cdd9b18488d84eca35af6009c7d7e390d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-08-25 08:58:59 +0200

    Update Base.c

--- 0000/Base.c
+++ 0000/Base.c
@@ -15,6 +15,17 @@ __main(){
     printf("\nCube of(Byval): %d is %d", num, ans);
 }
 
+void Pattern(){
+    for(int i = 1; i <= 3; ++i){
+        for(int j = 1; j <= 3; ++j){
+            for(int k = 1; k <= 3; k++)
+                printf("*");
+            puts(" ");
+        }
+        puts(" ");
+    }
+}
+
 //Access Element
 __main(){
     int x = 7;
