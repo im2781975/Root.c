691ab5818c419109dd0ecc251ae00ae34f7fbaf6
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-08-29 17:01:38 +0200

    Create 0010.c

---
+++ ARRAY/0010.c
@@ -0,0 +1,19 @@
+int printLeader(int *arr, int n){
+    int j;
+    for(int i = 0; i < n; i++){
+        for(j = i + 1; j < n; j++){
+            if(arr[i] <= arr[j])
+                break;
+        }
+        if(j == n)
+            printf("%d ", arr[i]);
+    }
+}
+__main(){
+    int n; scanf("%d", &n);
+    int arr[n + 2];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    printLeader(arr, n);
+}
+/***/
