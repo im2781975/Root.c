6693ac09e51dfd7af4b4d1c8b7dd696fa239e83d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-08-29 19:39:01 +0200

    Update 0010.c

--- ARRAY/0010.c
+++ ARRAY/0010.c
@@ -16,4 +16,21 @@ __main(){
         scanf("%d", &arr[i]);
     printLeader(arr, n);
 }
+void Leader(){
+    int n; scanf("%d", &n);
+    int arr[n + 2];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    for(int i = 0; i < n; i++){
+        int flag = 1;
+        for(int j = i + 1; j < n; j++){
+            if(arr[i] <= arr[j]){
+                flag = 0;
+                break;
+            }
+        }
+        if(flag)
+            printf("%d ", arr[i]);
+    }
+}
 /***/
