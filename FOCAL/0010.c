7b2f7e1efe02fca59ed6b3c1024be14e35a75ddc
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-08-31 13:47:57 +0200

    Update 0010.c

--- FOCAL/0010.c
+++ FOCAL/0010.c
@@ -34,3 +34,16 @@ void Sequential(){
         printf("\n");
     }
 }
+/***/
+void PrintM(){
+    int n; scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if((j == 0 || j == n - 1) || (i % 2 != 0 && j % 2 != 0 && i <= n/2) || (i % 2 == 0 && j % 2 == 0 && i <= n/2 && i != 0))
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+}
