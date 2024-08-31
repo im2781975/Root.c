bebe2ea8b0a5ab01e4f6ca54e9ec70aed32a1220
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-08-31 15:23:34 +0200

    Update 0010.c

--- FOCAL/0010.c
+++ FOCAL/0010.c
@@ -59,6 +59,18 @@ void PrintM(){
         printf("\n");
     }
 }
+void PrintM(){
+    int n; scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(j <= i || j >= n - 1 - i)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+}
 /***/
 void Rombo(){
     int n; scanf("%d", &n);
