ae6b5fc1edbb7d657a4a2c895e1a351684260812
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-08-31 15:09:25 +0200

    Update 0010.c

--- FOCAL/0010.c
+++ FOCAL/0010.c
@@ -47,3 +47,28 @@ void PrintM(){
         printf("\n");
     }
 }
+void PrintM(){
+    int n; scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if((j == 0 || j == n - 1) || (i == j || j == n - 1 - i) && i < n/2 )
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+}
+/***/
+void Rombo(){
+    int n; scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < 2*n; j++){
+            if(j <= n - 2 - i || j >= 2*n - i)
+                printf(" ");
+            else
+                printf("*");
+        }
+        printf("\n");
+    }
+}
