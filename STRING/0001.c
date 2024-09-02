e3b3d62cfaaa08708b638df9b532a42edb18f9ed
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-09-02 08:20:49 +0200

    Update 0001.c

--- STRING/0001.c
+++ STRING/0001.c
@@ -1,3 +1,68 @@
+/***/
+void strCpy(char *str, char *ing, int n){
+    int i = 0, j = 0;
+    while(str[i] != '\0'){
+        if(i == n)
+            break;
+        ing[j] = str[i];
+        i++; j++;
+    }
+    ing[j] = '\0';
+    printf("copied string is: %s", ing);
+}
+__main(){
+    char str[32], ing[32];
+    int n; scanf("%s%d", &str, &n);
+    strCpy(str, ing, n);
+}
+/***/
+/*
+void strCat(char *str, char *ing, int n){
+    int i = 0, j = 0, len = 0;
+    while(str[i] != '\0'){
+        if(i == n)
+            break;
+        len++; i++;
+    }
+    while(ing[j] != '\0'){
+        str[len + j] = ing[j];
+        j++;
+    }
+    printf("After Concat string is: %s", str);
+}*/
+void strCat(char *str, char *ing, int n){
+    int i = 0, j = 0, len = 0;
+    while(str[i] != '\0'){
+        len++; i++;
+    }
+    while(ing[j] != '\0'){
+        if(j == n)  
+            break;
+        str[len + j] = ing[j];
+        j++;
+    }
+    printf("After Concat String is: %s", str);
+}
+__main(){
+    char str[32] = "molla", ing[] = "vai";
+    int n = 3;
+    strCat(str, ing, 2);
+}
+/***/
+void strrev(char *str, char *ing, int len){
+    int i = 0, j = 0;
+    while(str[i]!= '\0'){
+        ing[j] = str[len - 1];
+        i++; j++; len--;
+    }
+    printf("Reverse of %s is: %s", str, ing);
+}
+__main(){
+    char str[30] = "i am", ing[30];
+    int len = strlen(str);
+    strrev(str, ing, len);
+}
+/***/
 // copy String
 void copy1(char *a, char *b){
     for(int i = 0; (a[i] = b[i])!= '\0'; ++i){}
