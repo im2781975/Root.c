76ebdce0bea43383f7a58fa190760284f0172652
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-09-02 10:09:24 +0200

    Update 0001.c

--- STRUCT/0001.c
+++ STRUCT/0001.c
@@ -8,3 +8,16 @@ int main(){
     x.age = 24; x.salary = 698.78;
     printf("Name : %s\nAge : %d\nsalary : %0.2f",x.name, x.age, x.salary);
 }
+/***/
+struct Complex{
+    int real; float img;
+};
+struct num{
+    struct Complex comp; int integer;
+}Acc;
+int main(){
+    Acc.comp.real = 25;
+    Acc.comp.img = 3.789;
+    Acc.integer = 67;
+    printf("Imaginary part: %0.3f\nReal part: %d\nInteger  part: %d", Acc.comp.img, Acc.comp.real, Acc.integer);
+}
