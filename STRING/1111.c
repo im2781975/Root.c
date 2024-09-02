859ffffc5bef78966ebf4e271f3b823635084694
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-09-02 07:02:00 +0200

    Update 1111.c

--- STRING/1111.c
+++ STRING/1111.c
@@ -16,6 +16,7 @@ __main(){
     strCpy(str, ing, n);
 }
 /***/
+/*
 void strCat(char *str, char *ing, int n){
     int i = 0, j = 0, len = 0;
     while(str[i] != '\0'){
@@ -28,10 +29,23 @@ void strCat(char *str, char *ing, int n){
         j++;
     }
     printf("After Concat string is: %s", str);
+}*/
+void strCat(char *str, char *ing, int n){
+    int i = 0, j = 0, len = 0;
+    while(str[i] != '\0'){
+        len++; i++;
+    }
+    while(ing[j] != '\0'){
+        if(j == n)  
+            break;
+        str[len + j] = ing[j];
+        j++;
+    }
+    printf("After Concat String is: %s", str);
 }
 __main(){
     char str[32] = "molla", ing[] = "vai";
     int n = 3;
-    strCat(str, ing, 4);
+    strCat(str, ing, 2);
 }
 /***/
