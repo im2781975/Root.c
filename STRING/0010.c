e903092a5ef86209486c164c35f3660610008377
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-09-02 12:24:01 +0200

    Update 0010.c

--- STRING/0010.c
+++ STRING/0010.c
@@ -8,3 +8,23 @@ void String(){
         printf("%s", str[i]);
 }
 /***/
+#define SZ 128
+__main(){
+    char str[10][10];
+    int i = 0, total = 0;
+    while(i < 10){
+        printf("Input line %d: ", i + 1);
+        if (fgets(str[i], SZ, stdin) != NULL){
+            //Remove newline if present
+            str[i][strcspn(str[i], "\n")] = '\0';
+            i++;
+        }
+        else
+            break;
+    }
+    total = i;
+    for(int i = 0; i < total; i++)
+        printf(" %s\n", str[i]);
+    puts("\n");
+}
+/***/
