b69758394cd13e626e7e9b92872f648eb5e92ff7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-02-19 21:49:16 +0600

    Update .c

--- ARRAY/.c
+++ ARRAY/.c
@@ -8,3 +8,24 @@ int main(){
     printf("%u", sizeof(arr));
     printf("%u", getsize(arr));
 }
+#include<stdio.h>
+//Access Elements
+int main(){
+    int arr[] = {2, 3, 1, 5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int *ptr = arr;
+    for(size_t i = 0; i < n; i++)
+        printf("arr[%u] = %d\t", i, arr[i]);
+    puts("");
+    for(size_t i = 0; i < n; i++)
+        printf("ptr[%u] = %d\t", i, ptr[i]);
+    puts("");
+    for(size_t i = 0; i < n; i++)
+        printf("*(arr + %u) = %d\t", i, *(arr + i));
+    puts("");
+    for(size_t i = 0; i < n; i++)
+        printf("(ptr + %u) = %d\t", i, *(ptr + i));
+    puts("");
+    for(size_t i = 0; i < n; i++)
+        printf("(ptr + %u) = %d\t", (ptr + i), ptr[i]);
+}
