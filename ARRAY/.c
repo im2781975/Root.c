79be183ecf0fd08ea5b9562acb8e644f419e6725
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-02-22 09:57:01 +0100

    Update .c

--- ARRAY/.c
+++ ARRAY/.c
@@ -262,3 +262,30 @@ int main(){
         puts("");
     }
 }
+#include<stdio.h>
+//Print median
+int main(){
+    int n; scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    for(int i = 0; i < n - 1; i++){
+        int flag = 0;
+        for(int j = 0; j < n - i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                int tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+                flag = 1;
+            }
+        }
+        if(flag == 0)
+            break;
+    }
+    if (n % 2 != 0)
+        printf("%d\n", arr[n / 2]);
+    else {
+        double median = (arr[n / 2] + arr[(n / 2) - 1]) / 2.0;
+        printf("%.3f\n", median);
+    }
+}
