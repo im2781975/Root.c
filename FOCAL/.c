f1545572c2470b6d2c1012f8a7f0a5f95e8d3b5f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-02-24 13:28:57 +0100

    Update .c

--- FOCAL/.c
+++ FOCAL/.c
@@ -16,3 +16,27 @@ int main(){
     int num; scanf("%d", &num);
     func(num);
 }
+#include<stdio.h>
+int a, b, c = 5, d = 7;
+int func1(int a, int b){
+    c = a + b;
+    return c * 2;
+}
+int func2(int c){
+    c += a; a *= 2;
+    return c;
+}
+int func3(int d){
+    c = d + 10;
+    b = func2(c);
+}
+int main(){
+    int a = 1, b = 3; 
+    printf("%d %d %d %d\n", a, b, c, d);
+    a = func1(a, b);
+    printf("%d %d %d %d\n", a, b, c, d);
+    d = func2(c);
+    printf("%d %d %d %d\n", a, b, c, d);
+    func3(b);
+    printf("%d %d %d %d\n", a, b, c, d);
+}
