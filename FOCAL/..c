3e41d6337da88b9b4cccdd8f7887f313e76bbc9b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-02-27 19:46:21 +0100

    Update ..c

--- FOCAL/..c
+++ FOCAL/..c
@@ -41,3 +41,35 @@ int main(){
         puts("");
     }
 }
+#include<stdio.h>
+//ROMBUS
+int main(){
+    int n; scanf("%d", &n);
+    //Rombus
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < 2*n; j++){
+            if(j <= n - 2 - i || j >= 2*n - i) printf(" ");
+            else printf("*");
+        }
+        puts("");
+    }
+    //Square
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(i == 0 || i == n - 1 || j == 0 || j == n - 1) printf("*");
+            else printf(" ");
+        }
+        puts("");
+    }
+    //Pattern
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(j == 1) printf("%d ", i);
+            else if(j == n) printf("%d ", n - i);
+            else if(i == 1 || i == n) printf("+");
+            else if(i == (n/2) + 1 && j == (n/2) + 1) printf("*");
+            else printf(" ");
+        }
+        puts("");
+    }
+}
