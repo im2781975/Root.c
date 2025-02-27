5cd77036c6ea8a1ce1c617d33e851530a7b80ec6
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-02-27 19:29:15 +0100

    Create ..c

---
+++ FOCAL/..c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int n; scanf("%d", &n);
+    //Sequential
+    for(int i = 1; i <= n; i++){
+        for(int j = n; j > 1; j--){
+            if(i > n - j + 1) printf("_");
+            else printf("%d", j);
+        }
+        for(int j = 1; j <= n; j++){
+            if(j >= n - i + 2) printf(" ");
+            else printf("%d", j);
+        }
+        puts("");
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = n; j > 1; j--){
+            if(i >= j) printf("%d", j);
+            else printf("_");
+        }
+        for(int j = 1; j <= n; j++){
+            if(i >= j) printf("%d", j);
+            else printf(" ");
+        }
+        puts("");
+    }
+}
