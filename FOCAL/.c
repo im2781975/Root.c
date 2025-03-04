4aea806406f5d5124a192d09d4464928e807a7c3
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-03-04 09:35:58 +0100

    Update .c

--- FOCAL/.c
+++ FOCAL/.c
@@ -2350,3 +2350,29 @@ int main(){
     printf("%d", printSum(1, n));
     printf("%d", DigitSum(0, n, arr));
 }
+#include<stdio.h>
+int print(int n){
+    if(n > 0){
+        print(n - 1);
+        printf("%d ", n);
+    }
+}
+int printRev(int n){
+    if(n > 0){
+        printf("%d ", n);
+        printRev(n - 1);
+    }
+}
+int fact(int n){
+    if(n > 0) return n * fact(n - 1);
+    else return 1;
+}
+int sum(int n){
+    if(n > 0)
+        return n + sum(n - 1);
+    return 0;
+}
+int main(){
+    int n; scanf("%d", &n);
+    printf("%d\n%d\n%d\n%d\n", sum(n), fact(n), printRev(n), print(n));
+}
